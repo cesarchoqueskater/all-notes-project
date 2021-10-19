7f85ca31af627536d3d575d2865437b960c318d1
cchoqueh <cesarchoqueskater@gmail.com>
2021-10-18 23:09:11 -0500

    Se agrega llamada a script para Markdown

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 import { addNoteMarkdown, saveNoteMarkdown, selectedPreviewMarkdown, homeContent } from './addNote.js'
+import textConvertToMarkdown from './markedMarkdown.js'
 
 const $addNote = document.querySelector('#addNote')
 const $saveNote = document.querySelector('#saveNote')
@@ -9,3 +10,4 @@ $addNote.addEventListener('click', addNoteMarkdown)
 $saveNote.addEventListener('click', saveNoteMarkdown)
 $selectedPreview.addEventListener('click', selectedPreviewMarkdown)
 $home.addEventListener('click', homeContent)
+textConvertToMarkdown()
---
+++ js/markedMarkdown.js
@@ -0,0 +1,8 @@
+export default function textConvertToMarkdown() {
+    const textArea = document.getElementById("textAreaId");
+    const copyValue = document.getElementById("htmlResult");
+    textArea.addEventListener('keyup', e => {
+        const currentContent = e.target.value;
+        copyValue.innerHTML = marked(currentContent);
+    });
+}
